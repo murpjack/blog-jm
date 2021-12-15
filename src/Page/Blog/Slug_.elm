1e57063ee4da7f934cfbb1e3ea35cf54914d2d88
Jack <jackmurphy756@gmail.com>
2021-12-15 21:01:35 +0000

    create /Blog, /Blog/Slug_ route

---
+++ src/Page/Blog/Slug_.elm
@@ -0,0 +1,76 @@
+module Page.Blog.Slug_ exposing (Data, Model, Msg, page)
+
+import DataSource exposing (DataSource)
+import Head
+import Head.Seo as Seo
+import Page exposing (Page, PageWithState, StaticPayload)
+import Pages.PageUrl exposing (PageUrl)
+import Pages.Url
+import Path exposing (toAbsolute)
+import Shared
+import View exposing (View)
+
+
+type alias Model =
+    ()
+
+
+type alias Msg =
+    Never
+
+
+type alias RouteParams =
+    { slug : String }
+
+
+page : Page RouteParams Data
+page =
+    Page.prerender
+        { head = head
+        , routes = routes
+        , data = data
+        }
+        |> Page.buildNoState { view = view }
+
+
+routes : DataSource (List RouteParams)
+routes =
+    DataSource.succeed [ { slug = "my-post" } ]
+
+
+data : RouteParams -> DataSource Data
+data routeParams =
+    DataSource.succeed ()
+
+
+head :
+    StaticPayload Data RouteParams
+    -> List Head.Tag
+head static =
+    Seo.summary
+        { canonicalUrlOverride = Nothing
+        , siteName = "elm-pages"
+        , image =
+            { url = Pages.Url.external "TODO"
+            , alt = "elm-pages logo"
+            , dimensions = Nothing
+            , mimeType = Nothing
+            }
+        , description = "TODO"
+        , locale = Nothing
+        , title = "TODO title" -- metadata.title -- TODO
+        }
+        |> Seo.website
+
+
+type alias Data =
+    ()
+
+
+view :
+    Maybe PageUrl
+    -> Shared.Model
+    -> StaticPayload Data RouteParams
+    -> View Msg
+view maybeUrl sharedModel static =
+    View.placeholder static.routeParams.slug
